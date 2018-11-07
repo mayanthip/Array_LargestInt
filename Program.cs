8aca4b00aaf0a6e4e3ac3cd0a344cb5aaadab815
Mayanthi Perera <mayanthi@netwealth.com.au>
2018-11-07 15:45:36 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] number = {1,5,4,9,8,6,7,10,8};
+             int maxInt=0;
+            for (int i =0; i<number.Length;i++)
+            {
+                if(number[i]>maxInt)
+                maxInt=number[i]; 
+                
+            }
+            Console.WriteLine("The largest number  in the array is  {0}",maxInt );
+        }
+    }
+
+}
